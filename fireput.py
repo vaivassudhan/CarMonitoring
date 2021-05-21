aa33068888570e49ef1c64d829dcb407dc4f906e
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-05-21 12:33:56 +0530

    python prgms

---
+++ fireput.py
@@ -0,0 +1,24 @@
+import time
+from firebase import firebase
+fb = firebase.FirebaseApplication('https://scms-33eae-default-rtdb.firebaseio.com/', None)
+jsonlink="scms-33eae-default-rtdb/Car/-M_ElJUj2wV6MB9xW8u8"
+
+def updateSpeed(value):
+    fb.put(jsonlink,"Speed",value)
+    print("Speed : ",value)
+def updateFuel(value):
+    fb.put(jsonlink,"Fuel",value)
+    print("Fuel : ",value)
+
+def updateRPM(value):
+    fb.put(jsonlink,"RPM",value)
+    print("RPM : ",value)
+def updateTemp(value):
+    fb.put(jsonlink,"Temp",value)
+    print("Temp : ",value)
+
+for i in range(0,6):
+    updateSpeed(i)
+    updateRPM((i+3)*100)
+    time.sleep(0.5)
+    updateTemp(i+100)
